dccde59fd5368f789673eff413fefabd8b36edda
DhalinJR <dhalinjr@gmail.com>
2018-11-08 18:50:26 -0500

    CORS

--- src/main/java/com/is3/SUCWS/controller/Controlador.java
+++ src/main/java/com/is3/SUCWS/controller/Controlador.java
@@ -3,6 +3,7 @@ package com.is3.SUCWS.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import com.is3.SUCWS.service.LugarService;
 
 @RestController
 @RequestMapping("/suc")
+@CrossOrigin
 public class Controlador {
 
 	@Autowired
